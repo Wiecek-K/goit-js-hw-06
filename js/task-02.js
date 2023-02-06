dee17e669a11f9da25972f4c7d6bc011c0867304
Wiecek-K <krystiandwiecek@gmail.com>
2023-02-06 18:57:42 +0100

    end_task2

--- js/task-02.js
+++ js/task-02.js
@@ -5,4 +5,13 @@ const ingredients = [
   'Tomatos',
   'Herbs',
   'Condiments',
-];
+]
+
+const body = document.body
+const list = body.querySelector('#ingredients')
+ingredients.forEach((e) => {
+  const listItem = document.createElement('li')
+  listItem.classList.add('item')
+  listItem.textContent = e
+  list.append(listItem)
+})
