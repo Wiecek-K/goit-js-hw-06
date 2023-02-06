95cfda23bf1685806dbc57fa34ecd3f6c1a76c90
Wiecek-K <krystiandwiecek@gmail.com>
2023-02-06 20:01:18 +0100

    end_task3

--- js/task-03.js
+++ js/task-03.js
@@ -11,4 +11,13 @@ const images = [
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
-];
+]
+const body = document.body
+const list = body.querySelector('.gallery')
+const markup = images
+  .map(
+    (image) =>
+      `<li class="list-item"><img class="image" src="${image.url}" alt="${image.alt}" /></li>`,
+  )
+  .join('')
+list.insertAdjacentHTML('beforeend', markup)
