69b160eb10d7507ecb29da4a15a01e91dbf9cc8a
Wiecek-K <krystiandwiecek@gmail.com>
2023-02-08 17:41:25 +0100

    new_task10E

--- js/task-10.js
+++ js/task-10.js
@@ -7,14 +7,19 @@ const boxCreator = controls.querySelector('button[data-create]')
 const boxDestroyer = controls.querySelector('button[data-destroy]')
 
 const destroyBoxes = () => (boxes.innerHTML = '')
+let boxCnt = 0
 
 boxCreator.addEventListener('click', () => {
   for (let i = 0; i < amount.value; i++) {
     const div = document.createElement('div')
-    div.style.width = 30 + i * 10 + 'px'
-    div.style.height = 30 + i * 10 + 'px'
+    div.style.width = boxCnt * 10 + 30 + 'px'
+    div.style.height = boxCnt * 10 + 30 + 'px'
     div.style.backgroundColor = getRandomHexColor()
     boxes.append(div)
+    boxCnt++
   }
 })
-boxDestroyer.addEventListener('click', () => destroyBoxes())
+boxDestroyer.addEventListener('click', () => {
+  destroyBoxes()
+  boxCnt = 0
+})
