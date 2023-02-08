7d4526762057e69e14e7cd317d39d745d61e1401
Wiecek-K <krystiandwiecek@gmail.com>
2023-02-08 19:56:41 +0100

    up-task6

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const body = document.body
 const input = body.querySelector('#validation-input')
-input.addEventListener('blur', (event) => {
-  if (event.currentTarget.value.length === input.dataset.length-0) {
+input.addEventListener('input', (event) => {
+  if (event.currentTarget.value.length === input.dataset.length - 0) {
     input.classList.add('valid')
     input.classList.remove('invalid')
   } else {
