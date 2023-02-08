162d9096706ebc7b72bea9c54a8757a28903c66e
Wiecek-K <krystiandwiecek@gmail.com>
2023-02-08 02:04:33 +0100

    end_task9

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,8 @@
 const body = document.body
 const input = body.querySelector('#font-size-control')
 const text = body.querySelector('#text')
+text.style.fontSize = input.value + 'px'
 input.addEventListener('input', (event) => {
-  console.log(event.currentTarget.value)
+  //text.style.fontSize = input.value + 'px'
   text.style.fontSize = event.currentTarget.value + 'px'
 })
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,9 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const body = document.body
+const changeColorBnt = document.querySelector('.change-color')
+const colorDisplay = document.querySelector('.color')
+const getRandomHexColor = () => '#' + Math.random().toString(16).substr(2, 6)
+changeColorBnt.addEventListener('click', () => {
+  const currentColor = getRandomHexColor()
+  body.style.backgroundColor = currentColor
+  colorDisplay.textContent = currentColor
+})
