ecac060a8ab5d0c98270d68b3a23bb93124dc9c8
Wiecek-K <krystiandwiecek@gmail.com>
2023-02-09 20:00:02 +0100

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,23 +1,31 @@
-let counterValue = 0
-
-const body = document.body
-const counter = document.querySelector('#counter')
 const display = counter.querySelector('#value')
 const decrementBtn = document.querySelector('button[data-action="decrement"]')
 const incrementBtn = document.querySelector('button[data-action="increment"]')
 decrementBtn.disabled = true
 
-counter.addEventListener('click', () => {
-  decrementBtn.disabled = counterValue < 1 ? true : false
-})
+class watchDog {
+  #cntV
+  constructor(value) {
+    this.#cntV = value
+  }
+  get cntV() {
+    return this.#cntV
+  }
+  set cntV(nevValue) {
+    this.#cntV = nevValue
+    display.textContent = this.#cntV
+    decrementBtn.disabled = this.#cntV < 1 ? true : false
+  }
+}
+let watchCntV = new watchDog(0)
+
 decrementBtn.addEventListener('click', () => {
-  counterValue--
-  //   if (counterValue < 1) {
-  //     counterValue = 0
-  //   }
-  display.textContent = counterValue
+  watchCntV.cntV--
 })
 incrementBtn.addEventListener('click', () => {
-  counterValue++
-  display.textContent = counterValue
+  watchCntV.cntV++
+})
+
+document.addEventListener('keydown', () => {
+  watchCntV.cntV += 3
 })
