aca455f199391f5024469199fd6bad251e32ecc4
Wiecek-K <krystiandwiecek@gmail.com>
2023-02-11 03:01:33 +0100

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,30 +1,30 @@
-const display = counter.querySelector('#value')
-const decrementBtn = document.querySelector('button[data-action="decrement"]')
-const incrementBtn = document.querySelector('button[data-action="increment"]')
+const display = counter.querySelector("#value");
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
 
 class watchDog {
-  #cntV
-  constructor(value) {
-    this.cntV = value
-  }
-  get cntV() {
-    return this.#cntV
-  }
-  set cntV(nevValue) {
-    this.#cntV = nevValue
-    display.textContent = this.#cntV
-    decrementBtn.disabled = this.#cntV < 1 ? true : false
-  }
+	#cntV;
+	constructor(value) {
+		this.cntV = value;
+	}
+	get cntV() {
+		return this.#cntV;
+	}
+	set cntV(nevValue) {
+		this.#cntV = nevValue;
+		display.textContent = this.#cntV;
+		decrementBtn.disabled = this.#cntV <1?true:false;
+	}
 }
-let watchCntV = new watchDog(0)
+let watchCntV = new watchDog(0);
 
-decrementBtn.addEventListener('click', () => {
-  watchCntV.cntV--
-})
-incrementBtn.addEventListener('click', () => {
-  watchCntV.cntV++
-})
+decrementBtn.addEventListener("click", () => {
+	watchCntV.cntV--;
+});
+incrementBtn.addEventListener("click", () => {
+	watchCntV.cntV++;
+});
 
-document.addEventListener('keydown', () => {
-  watchCntV.cntV = watchCntV.cntV > 3 ? watchCntV.cntV - 3 : 0
-})
+document.addEventListener("keydown", () => {
+	watchCntV.cntV = watchCntV.cntV > 3 ? watchCntV.cntV - 3 : 0;
+});
