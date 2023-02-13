b022247603422deeaa6e9999e9076c5370fab259
Wiecek-K <krystiandwiecek@gmail.com>
2023-02-13 04:33:02 +0100

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,8 @@
-const body = document.body
-const input = body.querySelector('#font-size-control')
-const text = body.querySelector('#text')
-text.style.fontSize = input.value + 'px'
-input.addEventListener('input', (event) => {
-  //text.style.fontSize = input.value + 'px'
-  text.style.fontSize = event.currentTarget.value + 'px'
-})
+const body = document.body;
+const input = body.querySelector("#font-size-control");
+const text = body.querySelector("#text");
+
+text.style.fontSize = `${input.value}px`;
+input.addEventListener("input", (event) => {
+	text.style.fontSize = `${event.currentTarget.value}px`;
+});
